89ece54f34a50cfff17db5919386bfeafca87b03
Eva <eva.del.olmo.garcia@accenture.com>
2023-01-18 10:59:37 +0100

    Ejercicio circunferencia

---
+++ EjerciciosCurso/src/ejercicio5/Circunferencia.java
@@ -0,0 +1,19 @@
+package ejercicio5;
+
+public class Circunferencia {
+
+	public static void main(String[] args) {
+		
+		final double PI= Math.PI;
+		int radio= 23;
+		double longitud= 2*PI*radio;
+		double area= PI*(radio*radio);
+		
+		
+		System.out.println(longitud);
+		System.out.println(area);
+		
+
+	}
+
+}
